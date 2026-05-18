f6fb7fbc725fcda0bf8a636d7b4edf6da8b6dd29
karthik80v <v.karthik.erd@gmail.com>
2026-05-18 16:15:27 -0700

    Minor fix

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -8,5 +8,5 @@ export const API_CONFIG = {
   authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODc3LCJpYXQiOjE3Nzc2NDk3MDMsImV4cCI6MTgwMzU2OTcwM30.zI61VCAUPNvmEoImnyNa6Jy5cDjdv4_qlHKOx9copnk',
   userId: 877,
   requestTimeoutMs: 30000,
-  underValueThresholdPercent: 10,
+  underValueThresholdPercent: 8,
 };
